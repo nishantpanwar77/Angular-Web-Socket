c174fcab2be3e887621283c631a3e3dc10ef9d89
nishantinterwork <nishant@interwork.biz>
2020-10-28 15:15:05 +0530

    ws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ocpp-websocket';
+
+  url = 'wss://154759e9235e.ngrok.io?userID=159487909369334';
+
+  response: Subject<any>;
+  data: any;
+  mainData: any;
+  constructor(
+    private socketSrv: WebsocketService
+  ) {
+    this.response = this.socketSrv
+      .connect(this.url);
+
+    this.response.subscribe(res => {
+      this.mainData = res;
+      console.log(this.mainData.data);
+      this.data = JSON.parse(this.mainData.data);
+      console.log(JSON.parse(this.mainData.data));
+    })
+  }
 }
